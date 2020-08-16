a56e5ef7603cef7136ac3b333fa4ba9d600644ee
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-08-16 15:58:03 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
+import io from "socket.io-client";
+const socket = io("http://localhost:4000");
+
 function App() {
+  const [isButton1Disabled, setIsButton1Disabled] = useState(false);
+  socket.on('changeButton',()=>{
+    // console.log('changeButton')
+    setIsButton1Disabled(!isButton1Disabled);
+  })
+  function emitEvent(){
+    socket.emit('disableAlternate');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button disabled={isButton1Disabled} onClick={emitEvent}>Button 1</button> &nbsp;&nbsp;&nbsp;
+     <button disabled={!isButton1Disabled} onClick={emitEvent}>Button 2</button>
     </div>
   );
 }
